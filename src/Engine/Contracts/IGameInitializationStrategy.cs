af274f6f3a9cc66113457cb7e5ac101d3ebf7c56
vassildinev <vassildinev@gmail.com>
2015-09-17 13:09:04 +0300

    Added skeleton for Strategy Design Pattern

---
+++ src/Engine/Contracts/IGameInitializationStrategy.cs
@@ -0,0 +1,9 @@
+﻿namespace Minesweeper.Engine.Contracts
+{
+    using Boards.Contracts;
+
+    public interface IGameInitializationStrategy
+    {
+        void Initialize(IBoard board);
+    }
+}
---
+++ src/Engine/Initializations/StandardGameInitializationStrategy.cs
@@ -0,0 +1,14 @@
+﻿namespace Minesweeper.Engine.Initializations
+{
+    using System;
+    using Contracts;
+    using Boards.Contracts;
+
+    public class StandardGameInitializationStrategy : IGameInitializationStrategy
+    {
+        public void Initialize(IBoard board)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
