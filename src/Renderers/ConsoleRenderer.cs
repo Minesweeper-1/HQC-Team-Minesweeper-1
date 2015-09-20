dfb4e8d73def6c870c87b15172586f9d05e06b93
vassildinev <vassildinev@gmail.com>
2015-09-20 11:46:03 +0300

    Console colors refactoring

--- src/Renderers/ConsoleRenderer.cs
+++ src/Renderers/ConsoleRenderer.cs
@@ -28,9 +28,6 @@
             this.RenderLeftSidebar(board, row, col);
             this.RenderTopBar(board, row, col + 1);
             this.RenderBoardCells(board, row, col + 1);
-
-            // Lines for debugging purposes
-            // this.RenderRevealedBoard(board, row + board.Rows + 3, col);
         }
 
         private void RenderLeftSidebar(IBoard board, int row, int col)
@@ -67,35 +64,7 @@
                 for (int boardCol = 0; boardCol < board.Cols; boardCol++)
                 {
                     string charToRenderAsString = this.GetCellCharAsString(board.Cells[boardRow, boardCol]);
-                    if (charToRenderAsString == GlobalConstants.StandardUnrevealedBoardCellCharacter.ToString())
-                    {
-                        this.SetForegroundColor("dark cyan");
-                    }
-                    else if (charToRenderAsString == "0")
-                    {
-                        this.SetForegroundColor("magenta");
-                    }
-                    else if (charToRenderAsString == "1")
-                    {
-                        this.SetForegroundColor("blue");
-                    }
-                    else if (charToRenderAsString == "2")
-                    {
-                        this.SetForegroundColor("green");
-                    }
-                    else if (charToRenderAsString == "3")
-                    {
-                        this.SetForegroundColor("red");
-                    }
-                    else if (charToRenderAsString == "4")
-                    {
-                        this.SetForegroundColor("dark green");
-                    }
-                    else if (charToRenderAsString == "5")
-                    {
-                        this.SetForegroundColor("dark magenta");
-                    }
-
+                    this.SetCorrespondingForegroundColor(charToRenderAsString);
                     this.Render(charToRenderAsString + " ");
                     this.ResetForegroundColor();
                 }
@@ -104,56 +73,36 @@
             this.ResetForegroundColor();
         }
 
-        private void RenderRevealedBoard(IBoard board, int row, int col)
+        private void SetCorrespondingForegroundColor(string charToRenderAsString)
         {
-            this.RenderLeftSidebar(board, row, col);
-            this.RenderTopBar(board, row, col + 1);
-            this.RenderRevealedBoardCells(board, row, col + 1);
-        }
-
-        private void RenderRevealedBoardCells(IBoard board, int row, int col)
-        {
-            for (int boardRow = 0; boardRow < board.Rows; boardRow++)
+            if (charToRenderAsString == GlobalConstants.StandardUnrevealedBoardCellCharacter.ToString())
             {
-                this.SetCursorPosition(row + boardRow, col + GlobalConstants.LeftSidebarWidth);
-                for (int boardCol = 0; boardCol < board.Cols; boardCol++)
-                {
-                    string charToRenderAsString = this.GetCellContentValue(board.Cells[boardRow, boardCol]);
-                    if (charToRenderAsString == GlobalConstants.StandardUnrevealedBoardCellCharacter.ToString())
-                    {
-                        this.SetForegroundColor("dark cyan");
-                    }
-                    else if (charToRenderAsString == "0")
-                    {
-                        this.SetForegroundColor("magenta");
-                    }
-                    else if (charToRenderAsString == "1")
-                    {
-                        this.SetForegroundColor("blue");
-                    }
-                    else if (charToRenderAsString == "2")
-                    {
-                        this.SetForegroundColor("green");
-                    }
-                    else if (charToRenderAsString == "3")
-                    {
-                        this.SetForegroundColor("red");
-                    }
-                    else if (charToRenderAsString == "4")
-                    {
-                        this.SetForegroundColor("dark green");
-                    }
-                    else if (charToRenderAsString == "5")
-                    {
-                        this.SetForegroundColor("dark magenta");
-                    }
-
-                    this.Render(charToRenderAsString + " ");
-                    this.ResetForegroundColor();
-                }
+                this.SetForegroundColor(ConsoleColor.DarkCyan);
             }
-
-            this.ResetForegroundColor();
+            else if (charToRenderAsString == "0")
+            {
+                this.SetForegroundColor(ConsoleColor.Magenta);
+            }
+            else if (charToRenderAsString == "1")
+            {
+                this.SetForegroundColor(ConsoleColor.Blue);
+            }
+            else if (charToRenderAsString == "2")
+            {
+                this.SetForegroundColor(ConsoleColor.Green);
+            }
+            else if (charToRenderAsString == "3")
+            {
+                this.SetForegroundColor(ConsoleColor.Red);
+            }
+            else if (charToRenderAsString == "4")
+            {
+                this.SetForegroundColor(ConsoleColor.DarkGreen);
+            }
+            else if (charToRenderAsString == "5")
+            {
+                this.SetForegroundColor(ConsoleColor.DarkMagenta);
+            }
         }
 
         private string GetCellCharAsString(ICell cell)
@@ -176,69 +125,19 @@
             return cellCharAsString;
         }
 
-        private string GetCellContentValue(ICell cell)
-        {
-            string cellCharAsString = string.Empty;
-
-            ContentType cellContentType = cell.Content.ContentType;
-            switch (cellContentType)
-            {
-                case ContentType.Number:
-                    cellCharAsString = cell.Content.Value.ToString();
-                    break;
-                case ContentType.Bomb:
-                    cellCharAsString = "-";
-                    break;
-                default:
-                    break;
-            }
-
-            return cellCharAsString;
-        }
-
         public void SetCursorPosition(int row, int col)
         {
             Console.SetCursorPosition(col, row);
         }
 
-        public void SetForegroundColor(string color)
+        public void SetForegroundColor(ConsoleColor color)
         {
-            string processedColor = color.Trim().ToLower();
-            switch (processedColor)
-            {
-                case "red":
-                    Console.ForegroundColor = ConsoleColor.Red;
-                    break;
-                case "blue":
-                    Console.ForegroundColor = ConsoleColor.Blue;
-                    break;
-                case "green":
-                    Console.ForegroundColor = ConsoleColor.Green;
-                    break;
-                case "dark cyan":
-                    Console.ForegroundColor = ConsoleColor.DarkCyan;
-                    break;
-                case "magenta":
-                    Console.ForegroundColor = ConsoleColor.Magenta;
-                    break;
-                case "yellow":
-                    Console.ForegroundColor = ConsoleColor.Yellow;
-                    break;
-                case "dark green":
-                    Console.ForegroundColor = ConsoleColor.DarkGreen;
-                    break;
-                case "dark magenta":
-                    Console.ForegroundColor = ConsoleColor.DarkMagenta;
-                    break;
-                default:
-                    Console.ForegroundColor = ConsoleColor.White;
-                    break;
-            }
+            Console.ForegroundColor = color;
         }
 
         public void ResetForegroundColor()
         {
-            this.SetForegroundColor(string.Empty);
+            this.SetForegroundColor(ConsoleColor.White);
         }
 
         public void Clear()
