2a589443c9b09a0dc1aa55c0cde151ec7d57cea7
vassildinev <vassildinev@gmail.com>
2015-09-21 12:03:14 +0300

    Implemented scoreboard rendering

--- src/BoardOperators/Common/ShowScoreboardCommand.cs
+++ src/BoardOperators/Common/ShowScoreboardCommand.cs
@@ -1,8 +1,15 @@
 ﻿namespace Minesweeper.BoardOperators.Common
 {
-    using Contracts;
+    using System.Collections.Generic;
+    using System.Linq;
+
     using Boards.Contracts;
+    using Contracts;
+    using DataManagers;
+    using Minesweeper.Common;
+    using Players;
     using Renderers.Contracts;
+    
 
     public class ShowScoreboardCommand : IBoardCommand
     {
@@ -17,7 +24,27 @@
 
         public void Execute(string commandText)
         {
-            // TODO: Implement Show Scoreboard command
+            // Fetch players from file
+            var jsonManager = new JsonManager();
+            var dataReader = new FileReader();
+
+            string leadersAsString = dataReader.ReadAllText(GlobalConstants.ScoreboardFilePath);
+            List<Player> leaders = jsonManager.Parse<List<Player>>(leadersAsString);
+
+            // Sort leaders by score just in case
+            IOrderedEnumerable<Player> sortedLeaders = leaders.OrderBy(player => -player.Score);
+
+            // Render players and scores in a user-friendly manner
+            // TODO: Extract as a method in the IRenderer interface
+            //this.renderer.RenderLine(line: "");
+            this.renderer.RenderLine(line: "===============");
+            this.renderer.RenderLine(line: "  SCOREBOARD  ");
+            this.renderer.RenderLine(line: "===============");
+            foreach (var player in sortedLeaders)
+            {
+                string line = string.Format(format: "{0} {1}", arg0: player.Name, arg1: player.Score);
+                this.renderer.RenderLine(line);
+            }
         }
     }
 }
