93d50cbca81aceaf9e2c2950123b78cb57e9d183
PavelNPavlov <pavelnapavlov@gmail.com>
2015-09-30 20:01:56 +0300

    Wpf general ui functionality Left, Right Mouse Clicks

--- src/MineSweeper WPF/App.xaml.cs
+++ src/MineSweeper WPF/App.xaml.cs
@@ -13,5 +13,6 @@ namespace MineSweeper_WPF
     /// </summary>
     public partial class App : Application
     {
+
     }
 }
--- src/MineSweeper WPF/MainWindow.xaml.cs
+++ src/MineSweeper WPF/MainWindow.xaml.cs
@@ -23,6 +23,64 @@ namespace MineSweeper_WPF
         public MainWindow()
         {
             InitializeComponent();
+
+            CreateButtons(10, 10);
+           
+        }
+
+        private void Left(object sender, RoutedEventArgs e)
+        {
+            var target = (Button)sender;
+            target.Content = 1;
+            
+        }
+
+        private void Right(object sender, RoutedEventArgs e)
+        {
+            var target = (Button)sender;
+            target.Content = 2;
+        }
+
+        private void CreateButtons(int x, int y)
+        {
+            Grid grid = Main;
+
+            List<ColumnDefinition> cols = new List<ColumnDefinition>();
+
+            for (int i = 0; i < x; i++)
+            {
+                var rowDef = new RowDefinition();
+                rowDef.Height = GridLength.Auto;
+                grid.RowDefinitions.Add(rowDef);
+
+
+            }
+
+            for (int i = 0; i < y; i++)
+            {
+                var colDef = new ColumnDefinition();
+                colDef.Width = GridLength.Auto;
+                grid.ColumnDefinitions.Add(colDef);
+
+
+            }
+
+            for (int i = 0; i < x; i++)
+            {
+                for (int j = 0; j < y; j++)
+                {
+                    var button = new Button();
+                    //button.Content = i * y + j;
+                    button.Width = 30;
+                    button.Height = 30;
+                    button.Click += Left;
+                    button.MouseRightButtonDown += Right;
+                    grid.Children.Add(button);
+                    Grid.SetColumn(button, j);
+                    Grid.SetRow(button, i);
+                }
+            }
+         
         }
     }
 }
