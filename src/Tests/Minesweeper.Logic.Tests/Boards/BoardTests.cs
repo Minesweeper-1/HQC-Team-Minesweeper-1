146ea0bb4433fb8b858f3b6e5d2eaf1c855502b6
V.Dinev <vassildinev@gmail.com>
2015-10-10 19:36:00 +0300

    100% code coverage for Board

--- src/Tests/Minesweeper.Logic.Tests/Boards/BoardTests.cs
+++ src/Tests/Minesweeper.Logic.Tests/Boards/BoardTests.cs
@@ -1,47 +1,199 @@
-﻿namespace Minesweeper.Logic.Tests.Boards
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+namespace Minesweeper.Logic.Tests.Boards
 {
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
     using System;
     using System.Collections.Generic;
 
-    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using Common;
     using Logic.Boards;
     using Logic.Boards.Contracts;
     using Logic.Boards.Settings.Contracts;
-    using Common;
+    using Logic.Cells.Contracts;
+    using Moq;
+    using Logic.Cells;
+    using Logic.Contents;
 
     [TestClass]
     public class BoardTests
     {
         [TestMethod]
-        public void TestingChangeBoardState()
+        public void BoardConstructorShouldSetProvidedParameters()
         {
-            var boardSettings = new Mock<BoardSettings>(9, 9, 3);
-            Mock<Board> board = new Mock<Board>(boardSettings.Object, new List<IBoardObserver>());
-            board.CallBase = true;
-            var ta = board.Object;
-            var nootif = new Notification("B", BoardState.Open);
-            ta.ChangeBoardState(nootif);
-            Assert.AreEqual(ta.BoardState, BoardState.Open);
+            var settings = new EasyBoardSettings();
+            var subscribers = new List<IBoardObserver>()
+            {
 
+            };
+
+            var board = new Board(settings, subscribers);
+
+            Assert.AreEqual(settings.Rows, board.Rows);
+            Assert.AreEqual(settings.Cols, board.Cols);
+            Assert.AreEqual(settings.NumberOfBombs, board.NumberOfMines);
+            Assert.AreEqual(subscribers, board.Subscribers);
+            Assert.AreEqual(BoardState.Open, board.BoardState);
+            Assert.AreNotEqual(default(ICell[,]), board.Cells);
         }
 
         [TestMethod]
-        public void ChangedBoardStateShouldThowEror()
+        public void SubscribeShouldAddTheObjectToTheBoardSubscribers()
         {
-            var boardSettings = new Mock<BoardSettings>(9, 9, 3);
-            Mock<Board> board = new Mock<Board>(boardSettings.Object, new List<IBoardObserver>());
-            board.CallBase = true;
-            var ta = board.Object;
-            var nootif = new Notification("B", BoardState.Open);
-            ta.ChangeBoardState(nootif);
-            try
-            {
-                Assert.AreEqual(ta.BoardState, BoardState.Closed);
-                Assert.Fail();
-            }
-            catch (Exception)
+            IBoardObserver mockedBoardObserver = new Mock<IBoardObserver>().Object;
+            var settings = new EasyBoardSettings();
+            var subscribers = new List<IBoardObserver>()
             {
 
+            };
+
+            var board = new Board(settings, subscribers);
+            board.Subscribe(mockedBoardObserver);
+
+            Assert.AreEqual(!default(bool), board.Subscribers.Contains(mockedBoardObserver));
+        }
+
+        [TestMethod]
+        public void UnsubscribeShouldRemoveTheObjectFromTheBoardSubscribers()
+        {
+            IBoardObserver mockedBoardObserver = new Mock<IBoardObserver>().Object;
+            var settings = new EasyBoardSettings();
+            var subscribers = new List<IBoardObserver>()
+            {
+                mockedBoardObserver
+            };
+
+            var board = new Board(settings, subscribers);
+
+            Assert.AreEqual(default(int) + 1, board.Subscribers.Count);
+
+            board.Unsubscribe(mockedBoardObserver);
+
+            Assert.AreEqual(default(int), board.Subscribers.Count);
+        }
+
+        [TestMethod]
+        public void CalculateSurroundingBombsOnEmptyBoardShouldReturnZero()
+        {
+            var settings = new EasyBoardSettings();
+            var subscribers = new List<IBoardObserver>()
+            {
+
+            };
+
+            var board = new Board(settings, subscribers);
+            this.FillBoard(board);
+
+            int result = board.CalculateNumberOfSurroundingBombs(default(int), default(int));
+
+            Assert.AreEqual(default(int), result);
+        }
+
+        [TestMethod]
+        public void CalculateSurroundingBombsOnCellWithBombShouldReturnZero()
+        {
+            var contentFactory = new ContentFactory();
+            var settings = new EasyBoardSettings();
+            var subscribers = new List<IBoardObserver>()
+            {
+
+            };
+
+            var board = new Board(settings, subscribers);
+            this.FillBoard(board);
+
+            board.Cells[default(int), default(int)].Content = contentFactory.GetContent(ContentType.Bomb);
+
+            int result = board.CalculateNumberOfSurroundingBombs(default(int), default(int));
+
+            Assert.AreEqual(default(int), result);
+        }
+
+        [TestMethod]
+        public void CalculateSurroundingBombsWithSurroundingBombShouldReturnOne()
+        {
+            var contentFactory = new ContentFactory();
+            var settings = new EasyBoardSettings();
+            var subscribers = new List<IBoardObserver>()
+            {
+
+            };
+
+            var board = new Board(settings, subscribers);
+            this.FillBoard(board);
+
+            board.Cells[default(int) + 1, default(int)].Content = contentFactory.GetContent(ContentType.Bomb);
+
+            int result = board.CalculateNumberOfSurroundingBombs(default(int), default(int));
+
+            Assert.AreEqual(default(int) + 1, result);
+        }
+
+        [TestMethod]
+        public void IsAlreadyShownShouldReturnFalseWhenCalledUponSealedCell()
+        {
+            var settings = new EasyBoardSettings();
+            var subscribers = new List<IBoardObserver>()
+            {
+
+            };
+
+            var board = new Board(settings, subscribers);
+            this.FillBoard(board);
+
+            bool result = board.IsAlreadyShown(default(int), default(int));
+
+            Assert.AreEqual(default(bool), result);
+        }
+
+        [TestMethod]
+        public void IsAlreadyShownShouldReturnTrueWhenCalledUponRevealedCell()
+        {
+            var settings = new EasyBoardSettings();
+            var subscribers = new List<IBoardObserver>()
+            {
+
+            };
+
+            var board = new Board(settings, subscribers);
+            this.FillBoard(board);
+            board.RevealCell(default(int), default(int));
+
+            bool result = board.IsAlreadyShown(default(int), default(int));
+
+            Assert.AreEqual(!default(bool), result);
+        }
+
+        [TestMethod]
+        public void ChangeBoardStateShouldChangeBoardStateAndNotifySubscribers()
+        {
+            var templateNotification = new Notification(string.Empty, BoardState.Open);
+            var mockedBoardObserver = new Mock<IBoardObserver>();
+            mockedBoardObserver.Setup(o => o.Update(templateNotification)).Verifiable();
+
+            var settings = new EasyBoardSettings();
+            var subscribers = new List<IBoardObserver>()
+            {
+                mockedBoardObserver.Object
+            };
+
+            var board = new Board(settings, subscribers);
+            board.ChangeBoardState(templateNotification);
+            mockedBoardObserver.Verify(o => o.Update(templateNotification), Times.AtMostOnce);
+
+        }
+
+        private void FillBoard(IBoard board)
+        {
+            var contentFactory = new ContentFactory();
+            for (var row = 0; row < board.Rows; row++)
+            {
+                for (int col = 0; col < board.Cols; col++)
+                {
+                    board.Cells[row, col] = new Cell()
+                        .SetContent(contentFactory.GetContent(ContentType.Empty))
+                        .SetState(CellState.Sealed)
+                        .GetContext();
+                }
             }
         }
     }
