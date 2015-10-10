2c90fe95d3ea9b29a9a310f1b37f69a08e3acdd6
dtopalov <dimiter.topalov79@gmail.com>
2015-10-10 19:43:13 +0300

    Logic.Common tests added

---
+++ src/Tests/Minesweeper.Logic.Tests/Common/CoordinateTests.cs
@@ -0,0 +1,28 @@
+﻿namespace Minesweeper.Logic.Common.Tests
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using Minesweeper.Logic.Common;
+
+    /// <summary>
+    /// Test class for the Coordinate class
+    /// </summary>
+    [TestClass]
+    public class CoordinateTests
+    {
+        [TestMethod]
+        public void CoordinatesWithEqualValuesForRowAndCallShouldBeEqual()
+        {
+            var firstCoordinate = new Coordinate(2, 2);
+            var secondCoordinate = new Coordinate(2, 2);
+            Assert.AreEqual(firstCoordinate, secondCoordinate);
+        }
+
+        [TestMethod]
+        public void CoordinatesWithDifferentValuesForRowOrCallShouldNotBeEqual()
+        {
+            var firstCoordinate = new Coordinate(2, 2);
+            var secondCoordinate = new Coordinate(2, 1);
+            Assert.AreNotEqual(firstCoordinate, secondCoordinate);
+        }
+    }
+}
---
+++ src/Tests/Minesweeper.Logic.Tests/Common/NotificationTests.cs
@@ -0,0 +1,20 @@
+﻿namespace Minesweeper.Logic.Common.Tests
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using Minesweeper.Logic.Common;
+
+    /// <summary>
+    /// Testclass for the Notification class
+    /// </summary>
+    [TestClass]
+    public class NotificationTests
+    {
+        [TestMethod]
+        public void NotificationShouldContainMessageAndState()
+        {
+            var testNotification = new Notification("testMessage", BoardState.Open);
+            Assert.AreEqual("testMessage", testNotification.Message);
+            Assert.AreEqual(BoardState.Open, testNotification.State);
+        }
+    }
+}
