5c5f3fa5a7fe965ce73771ca82c276c927eef47c
PavelNPavlov <pavelnapavlov@gmail.com>
2015-10-11 19:15:38 +0300

    Some tests

---
+++ tests/Minesweeper.Tests.Ui/InputProviders/InputProviderTests.cs
@@ -0,0 +1,45 @@
+﻿
+
+namespace Minesweeper.Tests.Ui.InputProviders
+{
+    using System;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using Minesweeper.Logic.InputProviders.Contracts;
+    using Moq;
+    using UI.Console.InputProviders.Contracts;
+
+    [TestClass]
+    public class InputProviderTests
+    {
+        private Mock<IConsoleInputProvider> MockedInputProvder()
+        {
+            var mockedInputProvider = new Mock<IConsoleInputProvider>();
+            mockedInputProvider.Setup(o => o.ReceiveInputLine()).Returns("a 1");
+            mockedInputProvider.Setup(o => o.IsKeyAvailable).Returns(true);
+            mockedInputProvider.Setup(o => o.GetKeyChar()).Returns(1);
+
+            return mockedInputProvider;
+        }
+
+        [TestMethod]
+        public void MockTestOFReceiceInputLieMethod()
+        {
+            var mockedInputProvider = MockedInputProvder();
+            Assert.AreEqual(mockedInputProvider.Object.ReceiveInputLine(), "a 1");
+        }
+
+        [TestMethod]
+        public void MockTestOFIsKeyAvailableProperty()
+        {
+            var mockedInputProvider = MockedInputProvder();
+            Assert.AreEqual(mockedInputProvider.Object.IsKeyAvailable, true);
+        }
+
+        [TestMethod]
+        public void MockTestOFGetKeyChar()
+        {
+            var mockedInputProvider = MockedInputProvder();
+            Assert.AreEqual(mockedInputProvider.Object.GetKeyChar(), 1);
+        }
+    }
+}
---
+++ tests/Minesweeper.Tests.Ui/Renderers/ConsoleRendererTest.cs
@@ -0,0 +1,45 @@
+﻿namespace Minesweeper.Tests.Ui.Renderers
+{
+    using System;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using Moq;
+    using UI.Console.Renderers.Contracts;
+
+    [TestClass]
+    public class ConsoleRendererTest
+    {
+        [TestMethod]
+        public void RenderLineMockest()
+        {
+            var mockRendere = new Mock<IConsoleRenderer>();
+            mockRendere.Setup(o => o.RenderLine("input")).Verifiable();
+
+            mockRendere.Object.RenderLine("input");
+
+            mockRendere.Verify();
+        }
+
+        [TestMethod]
+        public void RenderMockTest()
+        {
+            var mockRendere = new Mock<IConsoleRenderer>();
+            mockRendere.Setup(o => o.Render("input")).Verifiable();
+
+            mockRendere.Object.Render("input");
+
+            mockRendere.Verify();
+        }
+
+        [TestMethod]
+        public void RenderWelcomeScreenMockTest()
+        {
+            var mockRendere = new Mock<IConsoleRenderer>();
+            mockRendere.Setup(o => o.RenderWelcomeScreen("input")).Verifiable();
+
+            mockRendere.Object.RenderWelcomeScreen("input");
+
+            mockRendere.Verify();
+        }
+
+    }
+}
