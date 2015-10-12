347ce52c3812d6c6b59e914377cd1cd981c82b20
PavelNPavlov <pavelnapavlov@gmail.com>
2015-10-12 11:41:52 +0300

    Documentation for tests

--- tests/Minesweeper.UI.Console.Tests/MenuHandlers/MenuHandlerTests.cs
+++ tests/Minesweeper.UI.Console.Tests/MenuHandlers/MenuHandlerTests.cs
@@ -1,4 +1,7 @@
-﻿namespace Minesweeper.UI.Console.Tests.MenuHandlers
+﻿// <copyright file="MenuHandlerTests.cs" company="Team Minesweeper-1">
+// Copyright (c) The team. All rights reserved.
+// </copyright>
+namespace Minesweeper.UI.Console.Tests.MenuHandlers
 {
     using System;
     using System.Collections.Generic;
@@ -11,14 +14,27 @@
     using Microsoft.VisualStudio.TestTools.UnitTesting;
     using Moq;
 
+    /// <summary>
+    /// Tests for MenuHandler class
+    /// </summary>
     [TestClass]
     public class MenuHandlerTests
     {
+        /// <summary>
+        /// Console constant;
+        /// </summary>
         private const uint StdOutputHandle = 0xFFFFFFF5;
 
+        /// <summary>
+        /// Console creator
+        /// </summary>
+        /// <returns>Provides usable console</returns>
         [DllImport("kernel32")]
         public static extern bool AllocConsole();
 
+        /// <summary>
+        /// Test Constructor with mocked items
+        /// </summary>
         [TestMethod]
         public void ExpectNoExcpetionFromMenuHandlerConstructor()
         {
@@ -30,6 +46,9 @@
             var consoleMenuHandler = new ConsoleMenuHandler(mockedInputProvider.Object, mockedConsoleRenderer.Object, gameModes, 0, 0);
         }
 
+        /// <summary>
+        /// Test show function with mocked renderer and input provider
+        /// </summary>
         [TestMethod]
         public void ExpectNoExcpetionFromMenuHandlerShowMenu()
         {
@@ -43,6 +62,9 @@
             consoleMenuHandler.ShowSelections();
         }
 
+        /// <summary>
+        /// test selection menu when input is provided
+        /// </summary>
         [TestMethod]
         public void ExpectNoExcpetionFromMenuHandlerUserSelection()
         {
