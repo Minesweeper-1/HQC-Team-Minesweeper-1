8573253d24eb5c4ac061d3ba9d8d658ff92b9457
dtopalov <dimiter.topalov79@gmail.com>
2015-10-13 20:03:37 +0300

    stcop

--- tests/Minesweeper.UI.Console.Tests/GameTests.cs
+++ tests/Minesweeper.UI.Console.Tests/GameTests.cs
@@ -6,34 +6,32 @@ namespace Minesweeper.UI.Console.Tests
     using System;
     using System.Runtime.InteropServices;
     using System.Threading.Tasks;
-
-    using Console.InputProviders.Contracts;
     using Console.Renderers;
-
     using Microsoft.VisualStudio.TestTools.UnitTesting;
-
+    using Minesweeper.UI.Console.InputProviders.Contracts;
+    using Minesweeper.UI.Console.Renderers.Contracts;
     using Moq;
 
     /// <summary>
-    /// A test class for the Game class
+    /// A test class for the Game class.
     /// </summary>
     [TestClass]
     public class GameTests
     {
         /// <summary>
-        /// Standard output handle constant
+        /// Standard output handle constant.
         /// </summary>
         private const uint StdOutputHandle = 0xFFFFFFF5;
 
         /// <summary>
         /// Allocates a new console for the calling process.
         /// </summary>
-        /// <returns>True upon success and false otherwise</returns>
+        /// <returns>True upon success and false otherwise.</returns>
         [DllImport("kernel32")]
         public static extern bool AllocConsole();
 
         /// <summary>
-        /// Testing singleton functionality
+        /// Testing singleton functionality.
         /// </summary>
         [TestMethod]
         public void GameInstanceShouldCreateOnlyOneInstanceAndReturnIt()
@@ -44,7 +42,7 @@ namespace Minesweeper.UI.Console.Tests
         }
 
         /// <summary>
-        /// Game start with correct settings and allowed 200 microseconds execution time
+        /// Game start with correct settings and allowed 200 microseconds execution time.
         /// </summary>
         [TestMethod]
         public void TestStart()
@@ -76,11 +74,11 @@ namespace Minesweeper.UI.Console.Tests
         private static extern void SetStdHandle(uint stdHandle, IntPtr handle);
 
         /// <summary>
-        /// Limits method execution time. It is used to skip wait for user input
+        /// Limits method execution time. It is used to skip wait for user input.
         /// </summary>
-        /// <param name="timeSpan">Allow time of execution</param>
-        /// <param name="codeBlock">Code to be executed</param>
-        /// <returns>Code that will be terminated after time span</returns>
+        /// <param name="timeSpan">Allow time of execution.</param>
+        /// <param name="codeBlock">Code to be executed.</param>
+        /// <returns>Code that will be terminated after time span.</returns>
         private bool ExecuteWithTimeLimit(TimeSpan timeSpan, Action codeBlock)
         {
             try
